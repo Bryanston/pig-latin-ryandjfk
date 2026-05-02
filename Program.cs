146fd018aa3fb325b31fb367cc1f835b5022c7ea
ryandjfk <ma_r@bryanston.co.uk>
2026-05-02 09:15:08 +0000

    1

--- Program.cs
+++ Program.cs
@@ -4,8 +4,26 @@ class Program
 {
     static void Main()
     {
-        // Here is your empty program!
-        Console.Clear();
-        Console.WriteLine("Hello world!");
+        Console.WriteLine("Enter a sentence: ");
+        string userInput = Console.ReadLine();
+        string pigLatin = "";
+        string[] sentence = userInput.Split(' ');
+        foreach (string word in sentence)
+        {
+            pigLatin += PigLatin(word) + " ";
+        }
+        Console.WriteLine(pigLatin);
+    }
+
+    static string PigLatin(string word)
+    {
+        string result = "";
+        char firstLetter = word[0];
+
+        string substring = word.Substring(1);
+
+        result = substring + firstLetter + "ay";
+
+        return result;
     }
 }
